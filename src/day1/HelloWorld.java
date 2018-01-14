5364f650a07777820a96e24c43de74717ac13765
heniancheng <845802942@qq.com>
2018-01-14 18:29:12 +0800

    the first commit

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,10 @@
+package day1;
+
+/**
+ * Created by hechen on 2018/1/14.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
